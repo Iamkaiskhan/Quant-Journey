44d45c82e81a41a32d7dd5ad59ed723502af2eca
Iamkaiskhan <iamkaiskhan@gmail.com>
2026-07-28 21:19:36 +0530

    Update task2.py

--- month-01-python/day 18/task2.py
+++ month-01-python/day 18/task2.py
@@ -1,15 +1,38 @@
 # lets learn about error and exceptions
+def learning_exception():
+    try:
+        num=int(input('Enter the number:'))
+        assert num%2!=0                     # asssert checks and return our needed answer,if not then except is executed
+
+    except:
+        print('Not an even number')
+
+    else:                                # if we pass even number i.e if try block works then else will be executed
+        reciprocal=1/num
+        print(reciprocal)
+
+    finally:
+        print('This will execute,no matter what')
+
+
+
+
+## now lets learn about custom exceptions
+
+class InvalidAge(Exception):
+    'Raised when the age is less than 18'
+    pass
+
+number=18
 
 try:
-    num=int(input('Enter the number:'))
-    assert num%2==0                     # asssert checks and return if the number is even or not,if not even then except is executed
+    age=int(input("Enter your age:"))
+    if age<number:
+        raise InvalidAge
 
-except:
-    print('Not an even number')
+    else:
+        print("Eligible to vote")
 
-else:                                # if we pass even number i.e if try block works then else will be executed
-    reciprocal=1/num
-    print(reciprocal)
+except InvalidAge:
+    print('Not eligible for vote')
 
-finally:
-    print('This will execute,no matter what')
